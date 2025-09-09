22f76f820e6f32e84ed9aeac72c21875f05f727f
Lenka Limberková <limberkova.l@gmail.com>
2025-09-09 22:39:25 +0200

    ikonka krizku v navigaci mobil

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,15 @@
-document.getElementById('menuToggle').addEventListener('click', function() {
-    document.getElementById('myTopnav').classList.toggle('active');
+const menuToggle = document.getElementById('menuToggle');
+const topnav = document.getElementById('myTopnav');
+const icon = menuToggle.querySelector('i');
+
+menuToggle.addEventListener('click', function() {
+    topnav.classList.toggle('active');
+    // Toggle between hamburger and X icon
+    if (icon.classList.contains('fa-bars')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
 });
