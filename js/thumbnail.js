7fa0f9ace05c855a6b955cd635d37906709983b9
Lenka Limberková <limberkova.l@gmail.com>
2025-09-24 22:26:46 +0200

    uprava videi

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,14 +1,13 @@
 document.querySelectorAll('.video-wrapper').forEach(wrapper => {
-  wrapper.addEventListener('click', function () {
-    const videoId = this.getAttribute('data-id');
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
-    iframe.setAttribute('frameborder', '0');
-    iframe.setAttribute('allow', 'autoplay; encrypted-media');
-    iframe.setAttribute('allowfullscreen', 'true');
-    iframe.style.width = "100%";
-    iframe.style.height = "315px";
-    this.innerHTML = ""; // smaže thumbnail
-    this.appendChild(iframe);
+  wrapper.addEventListener('click', () => {
+    const iframe = wrapper.querySelector('iframe');
+    const thumb = wrapper.querySelector('.video-thumbnail');
+    const videoId = wrapper.getAttribute('data-id');
+
+    // nastavíme src s autoplay
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`;
+
+    iframe.style.display = 'block';
+    thumb.style.display = 'none';
   });
 });
